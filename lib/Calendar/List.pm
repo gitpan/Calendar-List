90816f7404d8fe02c71b565c0160c792cbae87d2
Barbie <barbie@missbarbell.co.uk>
2012-04-15 15:03:35 +0000

    Import of BARBIE/Calendar-List-0.24 from CPAN.
    gitpan-cpan-distribution: Calendar-List
    gitpan-cpan-version:      0.24
    gitpan-cpan-path:         BARBIE/Calendar-List-0.24.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/Calendar/List.pm
+++ lib/Calendar/List.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION @ISA %EXPORT_TAGS @EXPORT_OK @EXPORT);
-$VERSION = '0.23';
+$VERSION = '0.24';
 
 #----------------------------------------------------------------------------
 
@@ -244,19 +244,7 @@ sub _callist {
 		}
 	}
 
-#print STDERR "\n\n===[".scalar(each %returns)."]===\n\n";
 	return @returns	if($wantarray);
-#use Data::Dumper qw(DumperX);
-#open  FH, ">>trace.log" or die "cannot open file:$!\n";
-#print FH "STORED:\n".DumperX(\%returns)."\n";
-#close FH;
-#exit;
-#	while(my (@temp) = each %returns) {
-#		push @returns, @temp;
-#	}
-
-#	map { push @returns, $_->[0],$_->[1] } each %returns;
-#	return @returns;
 	return %returns;
 }
 
